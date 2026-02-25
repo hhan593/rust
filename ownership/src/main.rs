911c8947163270397d65a03bf3b5d5026c930d47
hh <1994824227@qq.com>
2026-02-25 11:47:43 +0800

    feat: 添加示例代码以演示Rust的所有权和引用概念

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -1,32 +1,27 @@
-// fn get_first(vr: &Vec<i32>) -> i32 {
-// <--- 注意这里的 &
-//     vr[0]
-// }
-
+/// 主函数 - 演示Rust的所有权和引用概念
+///
+/// 此函数演示了两种不同的数据类型（i32和String）在解引用和克隆时的不同行为：
+/// 1. 对于Copy类型的值（如i32），可以通过解引用来获取值的副本
+/// 2. 对于非Copy类型的值（如String），需要使用clone()方法来创建新的实例
 fn main() {
-    let mut v = vec![0, 1, 2]; // v 拥有这个 Vec
-    // let n = get_first(&v); // 传递的是 &v (借用)，不是 v (移动)
-    // println!("{} {}", n, v[1]); // v 依然有效，可以继续使用
+    // 演示对基本类型i32的引用和解引用
+    let v: Vec<i32> = vec![1, 2, 3];
+    let n_ref = &v[0];
+    let n: i32 = *n_ref;
 
-    let num: &i32 = &v[2]; // num 是一个引用，指向 v[2] 的值 2   此时编译器标记：v 被借用了，只要 num 还活着，v 就**不能变**！
-    // 编译器大喊：“停！num 还指着 v 里面的数据呢，你要是改了 v（比如扩容移动了内存），num 就会变成‘悬垂指针’（指向无效内存）！”
-    // 这就是 Rust 的借用规则：当你有一个引用指向某个数据时，你不能修改那个数据（除非你有一个可变引用），以保证内存安全。
+    // 演示对复杂类型String的引用和克隆
+    let v: Vec<String> = vec![String::from("hello_world")];
+    let s_ref = &v[0];
+    let s: String = s_ref.clone();
+    print!("{}", s);
+    println!(" - {}", n);
 
-    // 所以必须释放 num 的借用，才能修改 v。
-    println!("num: {}, v: {:?}", num, v); // num 依然有效，v 也依然有效，
-    v.push(3);
-    let changed_num = &v[2]; // 这里 num 依然有效，因为它只是一个引用，指向 v[2] 的值 2，num 的值是 2，而不是 v[2] 的地址，所以 num + 1 不会改变 num 的值，也不会影响 v[2] 的值
-    println!("changed_num: {}, v: {:?}", changed_num, v);
-    // println!("num: {}, v: {:?}", num, v); // num 依然有效，v 也依然有效，但是 num 的值已经改变
+    let mut name = (String::from("hello"), String::from("world"));
+    let first_name_ref = &name.0;
+    // let first_name_ref = get_first(&name);
+    name.1.push_str("hh");
+    println!("first_name_ref: {}, name: {:?}", first_name_ref, name.1);
 }
-
-// 关键点：函数签名 fn get_first(vr: &Vec<i32>)。
-// 符号 &：表示参数 vr 是一个引用（Reference）。
-// 含义：
-// 当你调用 get_first(&v) 时，你并没有把 v 的所有权交给函数。
-// 你只是借给了函数一个“查看权限”（不可变借用）。
-// 因为所有权还在 main 函数的 v 手里，所以函数执行完后，v 不会被释放（drop），可以在后面继续使用
-
-// 在 Rust 中，只有当所有权发生转移（Move）时，原变量才会失效并被释放。
-// 传值 func(v) -> 所有权转移 -> v 失效。
-// 传引用 func(&v) -> 所有权保留 -> v 依然有效
+// fn get_first(name: &(String, String)) -> &String {
+//     &name.0
+// }
