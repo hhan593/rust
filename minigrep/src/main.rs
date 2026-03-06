dff4d8dbeeca2ba5f2e5ef2e3deac891f0210110
hh <1994824227@qq.com>
2026-03-06 11:34:52 +0800

    feat: 更新 minigrep 项目，添加命令行参数处理和 Cargo.lock 文件

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,3 +1,13 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    // dbg!(args); //加上分号变成语句而不是表达式
+    // 程序的名字占据args的第一个元素 &args[0];
+    // 所以从索引为 1 的参数开始，获取传入的参数
+    let query = &args[1];
+    let filename = &args[2];
+
+    println!("Searching for {}", query);
+    println!("In file {}", filename);
 }
