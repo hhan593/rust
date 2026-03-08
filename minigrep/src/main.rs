17c84debe4dc604bd6472b52fb117b0946db7a6b
hh <1994824227@qq.com>
2026-03-08 23:37:54 +0800

    feat: 添加文件读取功能并更新命令行参数处理

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,4 +1,5 @@
 use std::env;
+use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -6,8 +7,19 @@ fn main() {
     // 程序的名字占据args的第一个元素 &args[0];
     // 所以从索引为 1 的参数开始，获取传入的参数
     let query = &args[1];
-    let filename = &args[2];
+    // let filename = &args[2];
 
-    println!("Searching for {}", query);
-    println!("In file {}", filename);
+    // println!("Searching for {}", query);
+    // println!("In file {}", filename);
+    let query = &args[1];
+    let file_path = &args[2];
+
+    println!("Searching for {query}");
+    println!("In file {file_path}");
+
+    println!("In file {file_path}");
+
+    let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
+
+    println!("With text:\n{contents}");
 }
