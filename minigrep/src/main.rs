d3f0c3b85892f1117c6077c4e640aec0e4714a6d
hh <1994824227@qq.com>
2026-03-09 00:02:39 +0800

    feat:解决代码报错，去掉引用符号

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -31,8 +31,8 @@ struct Config {
     file_path: String,
 }
 fn parge_params(args: &[String]) -> Config {
-    let query = &args[1].clone();
-    let file_path = &args[2].clone();
+    let query = args[1].clone();
+    let file_path = args[2].clone();
 
     Config { query, file_path }
 }
