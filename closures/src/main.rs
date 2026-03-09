8396293ce16e249df9703be2e18126e2b98dcd78
hh <1994824227@qq.com>
2026-03-09 20:31:53 +0800

    📝 docs:闭包相关知识

--- closures/src/main.rs
+++ closures/src/main.rs
@@ -1,72 +1,72 @@
-#[derive(Debug, PartialEq, Copy, Clone, Hash, Eq)]
-enum ShirtColor {
-    Red,
-    Green,
-    Blue,
-}
+// #[derive(Debug, PartialEq, Copy, Clone, Hash, Eq)]
+// enum ShirtColor {
+//     Red,
+//     Green,
+//     Blue,
+// }
 
-struct Inventory {
-    shirts: Vec<ShirtColor>,
-}
+// struct Inventory {
+//     shirts: Vec<ShirtColor>,
+// }
 
-impl Inventory {
-    // 这里的 giveaway 函数是闭包应用的核心场景
-    fn giveaway(&self, user_preference: Option<ShirtColor>) -> ShirtColor {
-        // unwrap_or_else 接收一个“闭包”作为参数
-        // || self.most_stocked() 就是闭包：
-        // 如果 user_preference 是 Some，就直接用里面的值；
-        // 如果是 None，才会执行 || 后的代码去计算库存最多的颜色。
-        user_preference.unwrap_or_else(|| self.most_stocked())
-    }
+// impl Inventory {
+//     // 这里的 giveaway 函数是闭包应用的核心场景
+//     fn giveaway(&self, user_preference: Option<ShirtColor>) -> ShirtColor {
+//         // unwrap_or_else 接收一个“闭包”作为参数
+//         // || self.most_stocked() 就是闭包：
+//         // 如果 user_preference 是 Some，就直接用里面的值；
+//         // 如果是 None，才会执行 || 后的代码去计算库存最多的颜色。
+//         user_preference.unwrap_or_else(|| self.most_stocked())
+//     }
 
-    fn most_stocked(&self) -> ShirtColor {
-        if self.shirts.is_empty() {
-            return ShirtColor::Red;
-        }
+//     fn most_stocked(&self) -> ShirtColor {
+//         if self.shirts.is_empty() {
+//             return ShirtColor::Red;
+//         }
 
-        let mut red = 0;
-        let mut green = 0;
-        let mut blue = 0;
+//         let mut red = 0;
+//         let mut green = 0;
+//         let mut blue = 0;
 
-        for color in &self.shirts {
-            match color {
-                ShirtColor::Red => red += 1,
-                ShirtColor::Green => green += 1,
-                ShirtColor::Blue => blue += 1,
-            }
-        }
+//         for color in &self.shirts {
+//             match color {
+//                 ShirtColor::Red => red += 1,
+//                 ShirtColor::Green => green += 1,
+//                 ShirtColor::Blue => blue += 1,
+//             }
+//         }
 
-        let max = red.max(green).max(blue);
+//         let max = red.max(green).max(blue);
 
-        // 返回逻辑：谁最大就给谁，优先级是 蓝 > 绿 > 红
-        if max == blue {
-            ShirtColor::Blue
-        } else if max == green {
-            ShirtColor::Green
-        } else {
-            ShirtColor::Red
-        }
-    }
-}
+// 返回逻辑：谁最大就给谁，优先级是 蓝 > 绿 > 红
+//         if max == blue {
+//             ShirtColor::Blue
+//         } else if max == green {
+//             ShirtColor::Green
+//         } else {
+//             ShirtColor::Red
+//         }
+//     }
+// }
 
-fn main() {
-    let store = Inventory {
-        shirts: vec![ShirtColor::Red, ShirtColor::Green, ShirtColor::Blue],
-    };
+// fn main() {
+//     let store = Inventory {
+//         shirts: vec![ShirtColor::Red, ShirtColor::Green, ShirtColor::Blue],
+//     };
 
-    // 情况1：用户说了他想要红色（Some）
-    let user_pref1 = Some(ShirtColor::Red);
-    let giveaway1 = store.giveaway(user_pref1);
-    println!("用户指定了 {:?}, 最终拿到: {:?}", user_pref1, giveaway1);
+// 情况1：用户说了他想要红色（Some）
+//     let user_pref1 = Some(ShirtColor::Red);
+//     let giveaway1 = store.giveaway(user_pref1);
+//     println!("用户指定了 {:?}, 最终拿到: {:?}", user_pref1, giveaway1);
 
-    // 情况2：用户没主见（None），触发了闭包执行逻辑
-    let user_pref2 = None;
-    let giveaway2 = store.giveaway(user_pref2);
-    println!(
-        "用户没选 ({:?}), 自动计算库存最多的颜色: {:?}",
-        user_pref2, giveaway2
-    );
-}
+// 情况2：用户没主见（None），触发了闭包执行逻辑
+//     let user_pref2 = None;
+//     let giveaway2 = store.giveaway(user_pref2);
+//     println!(
+//         "用户没选 ({:?}), 自动计算库存最多的颜色: {:?}",
+//         user_pref2, giveaway2
+//     );
+// }
 
 // ```
 
@@ -109,4 +109,14 @@ fn main() {
 
 // **你想看看闭包如何“捕获”外部变量（就像照相机拍照一样留住那一刻的变量值）的代码例子吗？**
 
-//闭包的类型推断和注释
+//闭包的多种写法
+
+fn main() {
+    let sqare = |x| x * x; //c此时不知道他的类型，也没法进行推断
+    println!("sqaer is {}", sqare(5)); //这是使用了5 ，rust就自己推断出了x的类型为i32
+
+    //类型注解的闭包
+
+    let add = |x: u32, y: u32| -> u32 { x + y };
+    println!("add is {}", add(9, 3))
+}
