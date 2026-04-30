2741fc63aa85fcb2485733e04962788110b37c73
hh <1994824227@qq.com>
2026-04-30 17:41:39 +0800

    ``` refactor(rust_project): 更新函数返回值示例代码
    移除了原有的func_copy_back函数和get_mess函数，
    添加了func_return_value和func_return_ref函数来演示
    返回值与返回引用的区别，并更新了main函数中的调用方式
    ```

--- rust_project/ch4/ch5_fn_back/src/main.rs
+++ rust_project/ch4/ch5_fn_back/src/main.rs
@@ -1,25 +1,22 @@
-fn func_copy_back() -> i32 {
-    let n = 42;
-    n
+//返回值和返回引用
+// 返回值和返回引用的区别在于，返回值会将数据复制一份，而返回引用则是返回数据的地址。返回引用可以避免不必要的数据复制，提高性能，但需要注意生命周期和所有权的问题。
+
+fn func_return_value() -> i32 {
+    return 10;
+}
+
+fn func_return_ref() -> &i32 {
+    let x = 10;
+    return &x;
 }
 
 fn func_non_copy_back() -> String {
     let s = String::from("hello");
     s
 }
-
-fn get_mess(mark: i32) -> &'static str {
-    if mark == 0 {
-        "😊😀"
-    } else {
-        "≧ ﹏ ≦😫"
-    }
-}
-
 fn main() {
-    let i = func_copy_back();
-    println!("{}", i);
-    let s = func_non_copy_back();
-    println!("{}", s);
-    println!("{}", get_mess(i));
+    let x = func_return_value();
+    println!("x = {x}");
+    let s = func_return_ref();
+    println!("s = {s}");
 }
