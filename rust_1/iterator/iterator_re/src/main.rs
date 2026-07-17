21a0b6896a78c1f4efea9523a1969d1d58d59362
hh <clhhan@163.com>
2026-07-17 17:40:36 +0800

    提交代码

--- rust_1/iterator/iterator_re/src/main.rs
+++ rust_1/iterator/iterator_re/src/main.rs
@@ -1,3 +1,22 @@
+// &[i32] &Vec
+
+fn sum_with_loop(arr: &[i32]) -> i32 {
+    let mut sum = 0;
+    for &item in arr {
+        sum += item
+    }
+    sum
+}
+
+fn sum_with_iter(arr: &[i32]) -> i32 {
+    let sum = arr.iter().sum();
+    sum
+}
 fn main() {
-    println!("Hello, world!");
+    const ARRAY_SIZE: usize = 1_000;
+    let array: Vec<i32> = (1..=ARRAY_SIZE as i32).collect();
+    let sum1 = sum_with_loop(&array);
+    println!("{}", sum1);
+    let sum2 = sum_with_iter(&array);
+    println!("{}", sum2)
 }
